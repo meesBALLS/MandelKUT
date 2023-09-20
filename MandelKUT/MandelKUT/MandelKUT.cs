eb9f26ceee9716cb1a6f4a5ecbb31ee50fce84cd
MeesBJV2 <90202167+MeesBJV2@users.noreply.github.com>
2023-09-20 15:05:15 +0200

    Begin

--- MandelKUT/MandelKUT/MandelKUT.cs
+++ MandelKUT/MandelKUT/MandelKUT.cs
@@ -1,5 +1,7 @@
 using System.Drawing;
 using System.Windows.Forms;
+using System;
+
 
 internal class MandelKUT : Form
 {
@@ -10,6 +12,19 @@ internal class MandelKUT : Form
         this.ClientSize = new Size(200, 100);
         this.Paint += this.Teken;
     }
+
+
+    public double divergerent (double ca, double cb, int max_iter = 50)
+    {
+        double za = 0;
+        double zb = 0; 
+        for (int i = 0; i < max_iter; i++)
+        {
+            za = Math.Pow(za, 2) + Math.Pow(zb, 2) + ca;
+            zb = 2*za*zb + cb;
+        }
+    }
+
     public void Teken(object o, PaintEventArgs pea)
     {
         Graphics gr = pea.Graphics;
