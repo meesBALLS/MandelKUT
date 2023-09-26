e38943383593e13bd7d409f4f2885f1069ec3e3f
Broken-mistake <f.e.stelling@gmail.com>
2023-09-26 18:20:56 +0200

    idk

--- MandelKUT/MandelKUT/MandelKUT.cs
+++ MandelKUT/MandelKUT/MandelKUT.cs
@@ -2,23 +2,28 @@ using System.Drawing;
 using System.Windows.Forms;
 using System;
 using System.Diagnostics;
+using System.Drawing.Drawing2D;
+
 
 internal class MandelKUT : Form
 {
-
     int size = 1080;
+    Point Muispunt = new Point(0, 0);
     Bitmap plaatje;
     Button renderKnop;
     TextBox tX, tY, tSchaal, tIter;
-    Label lblX, lblY, lblSchaal, lblIter;
-    int schaal, iteraties, centerx, centery;
+    Label lbX, lbY, lbSchaal, lbIter;
+    int schaal = 1, iteraties = 50 ;
+    double centerx, centery, factor;
+
+
     public MandelKUT()
     {
-        lblX = new Label(); lblY = new Label();
-        lblX.Location = new Point(170, 10); lblY.Location = new Point(170, 35);
-        lblX.Size = new Size(15, 20); lblY.Size = new Size(15, 20);
-        lblX.Text = "X:"; lblY.Text = "Y:";
-        Controls.Add(lblX); Controls.Add(lblY);
+        lbX = new Label(); lbY = new Label();
+        lbX.Location = new Point(170, 10); lbY.Location = new Point(170, 35);
+        lbX.Size = new Size(15, 20); lbY.Size = new Size(15, 20);
+        lbX.Text = "X:"; lbY.Text = "Y:";
+        Controls.Add(lbX); Controls.Add(lbY);
 
         tX = new TextBox(); tY = new TextBox();
         tX.Location = new Point(190, 10); tY.Location = new Point(190, 35);
@@ -27,19 +32,35 @@ internal class MandelKUT : Form
 
         tSchaal = new TextBox(); Controls.Add(tSchaal); tIter = new TextBox(); Controls.Add(tIter); tIter.Location = new Point(60, 45); tSchaal.Location = new Point(60, 10);
         renderKnop = new Button(); Controls.Add(renderKnop); renderKnop.Location = new Point(60, 110); renderKnop.Text = "bereken";
-        lblSchaal = new Label(); Controls.Add(lblSchaal); lblIter = new Label(); Controls.Add(lblIter);
-        lblSchaal.Location = new Point(20, 10); lblSchaal.Text = "schaal:";
-        lblIter.Location = new Point(20, 35); lblIter.Text = "max \naantal:";
-        lblIter.Size = new Size(50, 40);
+        lbSchaal = new Label(); Controls.Add(lbSchaal); lbIter = new Label(); Controls.Add(lbIter);
+        lbSchaal.Location = new Point(20, 10); lbSchaal.Text = "schaal:";
+        lbIter.Location = new Point(20, 35); lbIter.Text = "max \naantal:";
+        lbIter.Size = new Size(50, 40);
+
+        
 
         plaatje = new Bitmap(size / 2, size / 2);
+        plaatje.SetResolution(96.0F, 96.0F);
         this.Text = "MandelKUT";
         this.BackColor = Color.Aqua;
         this.ClientSize = new Size(size, size);
         this.Paint += this.Teken;
 
         renderKnop.Click += renderKnop_Click;
+        this.MouseClick += muisklik;
     }
+    void muisklik(object sender, MouseEventArgs mea)
+    {
+        Muispunt = mea.Location;
+        Console.WriteLine(Muispunt.ToString());
+        double Muisx = (Muispunt.X-size) / factor;
+        double Muisy = (Muispunt.Y-size) / factor;
+        tX.Text = Muisx.ToString(); tY.Text = Muisy.ToString();
+        /*centerx = Muisx;
+        centery = Muisy;
+        Invalidate();*/
+    }
+
 
 
 
@@ -67,15 +88,15 @@ internal class MandelKUT : Form
     {
         if (int.TryParse(tSchaal.Text, out schaal) &&
             int.TryParse(tIter.Text, out iteraties) &&
-            int.TryParse(tX.Text, out centerx) &&
-            int.TryParse(tY.Text, out centery)) 
+            double.TryParse(tX.Text, out centerx) &&
+            double.TryParse(tY.Text, out centery))
         {
-           
+
             Invalidate();
         }
         else
         {
-            MessageBox.Show("Please enter valid integers.");
+            MessageBox.Show("Please enter integers in all the boxes.");
         }
     }
 
@@ -84,26 +105,32 @@ internal class MandelKUT : Form
     {
         Stopwatch sw = Stopwatch.StartNew();
         Graphics gr = pea.Graphics;
-        double factor = 20 * (1 + schaal);
+        gr.SmoothingMode = SmoothingMode.AntiAlias;
+
+        factor = 20 * (1 + schaal);
         //double tussenstap = size / (4 * factor);//
-        double centerX = (double)centerx;
-        double centerY = (double)centery;
-        centerX /= 100.0;
-        centerY /= 100.0;
+
+        /*Color redColor = Color.FromArgb(255, 0, 0);*/
         for (int i = 0; i < plaatje.Width; i++)
         {
 
             for (int j = 0; j < plaatje.Height; j++)
             {
-
+                
                 double x = i;
-                x = (x - size / 4) / factor + centerX;
-
+                x = (x - size / 4) / factor + centerx;
+                
                 double y = j;
-                y = (y - size / 4) / factor + centerY;
-
+                y = (y - size / 4) / factor + centery;
+                Color redColor = Color.FromArgb(255, 0, 0);
                 int q = Divergerent(x + centerx, y + centery, iteraties);
                 //y=y/factor-tussenstap//
+
+
+
+                /*Color color = Color.FromArgb(255, collor, collor, collor);
+
+                plaatje.SetPixel(i, j, color);*/
                 if (q % 2 == 0)
                 {
                     plaatje.SetPixel(i, j, Color.Pink);
@@ -119,9 +146,10 @@ internal class MandelKUT : Form
 
             }
         }
-        gr.DrawImage(plaatje, size / 8, size / 8);
+        gr.DrawImage(plaatje, size / 16, size / 16);
         sw.Stop();
         Console.WriteLine(sw.Elapsed.ToString());
+        
     }
 
 
